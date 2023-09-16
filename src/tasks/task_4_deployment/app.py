d09eaa731122ef10cc6ab895f59da4a0059a4465
Soh Zong Xian <sotongszx@gmail.com>
2023-09-16 16:12:49 +0800

    [Added Features, and fix date formatting]

--- src/tasks/task_4_deployment/app.py
+++ src/tasks/task_4_deployment/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import matplotlib.dates as mdates
 import streamlit as st
 import matplotlib.pyplot as plt
 
@@ -12,7 +13,7 @@ with open('config.json', 'r') as f:
 
 data_columns = json_data['data_columns']
 now = datetime.now() - relativedelta(days=7)
-start = now - relativedelta(years=1)
+start = now - relativedelta(months=11)
 date_string_end = now.strftime('%Y-%m-%d')
 date_string_start = start.strftime('%Y-%m-%d')
 
@@ -48,31 +49,48 @@ total_hours = concat_df['precipitation_hours'].sum()
 concat_df['precipitation_rate'] = concat_df['precipitation_sum']/total_hours
 
 ##Streamlit app
-st.set_page_config(layout="wide", page_title="Weather Prediction", page_icon="⛅")
+st.set_page_config(page_title="Weather Prediction", page_icon="⛅")
 st.title('Karachi Pakistan Weather Prediction')
 if concat_df is not None:
-    st.write(concat_df)
+    display_df = concat_df[['temperature_2m_max', 'et0_fao_evapotranspiration', 'precipitation_rate']]
+    st.write("Current values")
+    st.write(display_df.tail(10))
+
+    st.write("Predicted future values - To be implemented")
 
     # Plot the first column
     st.subheader(f'Plot of temperature_2m_max over time')
     fig1, ax1 = plt.subplots()
     ax1.plot(concat_df.index, concat_df['temperature_2m_max'])
+    # Set the locator
+    locator = mdates.MonthLocator()  # every month
+    fmt = mdates.DateFormatter('%b')
+
+    X = plt.gca().xaxis
+    X.set_major_locator(locator)
+    X.set_major_formatter(fmt)
     st.pyplot(fig1)
 
     # Plot the second column
     st.subheader(f'Plot of et0_fao_evapotranspiration over time')
     fig2, ax2 = plt.subplots()
     ax2.plot(concat_df.index, concat_df['et0_fao_evapotranspiration'])
+    locator = mdates.MonthLocator()  # every month
+    fmt = mdates.DateFormatter('%b')
+
+    X = plt.gca().xaxis
+    X.set_major_locator(locator)
+    X.set_major_formatter(fmt)
     st.pyplot(fig2)
 
     # Plot the third column
     st.subheader(f'Plot of precipitation_rate over time')
     fig3, ax3 = plt.subplots()
     ax3.plot(concat_df.index, concat_df['precipitation_rate'])
+    locator = mdates.MonthLocator()  # every month
+    fmt = mdates.DateFormatter('%b')
+
+    X = plt.gca().xaxis
+    X.set_major_locator(locator)
+    X.set_major_formatter(fmt)
     st.pyplot(fig3)
-
-
-
-
-
-
